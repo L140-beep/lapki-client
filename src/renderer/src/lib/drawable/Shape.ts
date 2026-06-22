fdea08cb4a3ac171457cebf7853c1366a995de17
L140-beep <maksimklypin0@gmail.com>
2026-06-22 13:41:36 +0800

    Учет положения локальной истории при расчете ширины состояния

--- src/renderer/src/lib/drawable/Shape.ts
+++ src/renderer/src/lib/drawable/Shape.ts
@@ -93,6 +93,7 @@ export abstract class Shape extends EventEmitter<ShapeEvents> implements Drawabl
         ...this.children.getLayer(Layer.ChoiceStates),
         ...this.children.getLayer(Layer.Transitions),
         ...this.children.getLayer(Layer.Components),
+        ...this.children.getLayer(Layer.ShallowHistory)
       ] as Shape[];
 
       let rightChildren = children[0] as Shape;
